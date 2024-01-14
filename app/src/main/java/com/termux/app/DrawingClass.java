e09f346633c967f66d8f012d3417b419a7e63c87
Craig Comstock <craig@unreasonablefarm.org>
2024-01-14 10:45:58 -0600

    refactor to remove gesture stuff as much as possible from termuxactivity class

---
+++ app/src/main/java/com/termux/app/DrawingClass.java
@@ -0,0 +1,22 @@
+package com.termux.app;
+
+import android.graphics.Paint;
+import android.graphics.Path;
+
+public class DrawingClass {
+    Path DrawingClassPath;
+    Paint DrawingClassPaint;
+
+    public Path getPath() {
+        return DrawingClassPath;
+    }
+    public void setPath(Path path) {
+        this.DrawingClassPath = path;
+    }
+    public Paint getPaint() {
+        return DrawingClassPaint;
+    }
+    public void setPaint(Paint paint) {
+        this.DrawingClassPaint = paint;
+    }
+}
